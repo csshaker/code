ece2a301b6166d46cfd9f0c5366badcf06f57522
Shaker Ahmed <shakerahmed063@gmail.com>
2022-10-10 22:27:23 +0600

    division fnc added

--- hello.py
+++ hello.py
@@ -18,4 +18,10 @@ def subtraction(a, b):
 def addition(a, b):
 	return a + b
 
+def division(a, b):
+	try:
+		return a / b
+	except ZeroDivisionError:
+		print("cannot divide by zero")
+
 #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
