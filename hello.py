1b638c63ccffb5ae1e7d6a8ab94b7626669db042
Shaker Ahmed <shakerahmed063@gmail.com>
2022-10-16 15:52:28 +0600

    maxThree func added

--- hello.py
+++ hello.py
@@ -1,4 +1,3 @@
-
 def func_signup():
 	print("sign up")
 	
@@ -20,4 +19,14 @@ def division(a, b):
 	except ZeroDivisionError:
 		print("cannot divide by zero")
 
-#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
+def maxThree(num1, num2, num3):
+	if num1 > num2:
+		if num1 > num3:
+			return num1
+		else 
+			return num3
+	else:
+		if num2 > num3
+			return num2
+		else
+			return num3
