055cbe6e5a13a6632be3ed582525049dddedc6dd
Shaker Ahmed <shakerahmed063@gmail.com>
2022-10-27 00:07:24 +0600

    Triangle class added

--- Rectangle.py
+++ Rectangle.py
@@ -15,3 +15,13 @@ class Square:
 
 	def area(self):
 		return self.a * self.a 
+
+class Triangle:
+	def __init__(self, base, height):
+		self.base = base
+		self.height = height
+
+	def area(self):
+		return 0.5 * self.base * self.height
+
+		
